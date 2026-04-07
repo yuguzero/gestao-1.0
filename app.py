447e5d17f11bba7c978e33c461775d0bdb880dbc
yuguzero <isaque.dotto@gmail.com>
2026-04-07 11:54:15 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# "banco de dados" temporário (lista)
+brinquedos = []
+
 @app.route("/")
 def home():
-    return "Seu app tá rodando 🔥"
+    return render_template("index.html")
+
+# LISTAR brinquedos
+@app.route("/brinquedos", methods=["GET"])
+def listar_brinquedos():
+    return jsonify(brinquedos)
+
+# ADICIONAR brinquedo
+@app.route("/brinquedos", methods=["POST"])
+def adicionar_brinquedo():
+    dados = request.json
+    brinquedos.append(dados)
+    return {"mensagem": "Brinquedo adicionado!"}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
